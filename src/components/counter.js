3ce007c251f27eef86e3d3509582476297f80879
Sam <sam.fordham1@gmail.com>
2020-08-14 10:53:30 +0100

    It's working, in that the counters increase and decrease, but it's showing in the tab, not the page.

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,13 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Counter = () => {
-    const {count, setCount} = useState(0);
+    const [count, setCount] = useState(0);
+    useEffect(()=>{
+        document.title = 'Counter:' + count;
+    });
+
+    const Increase = () => {
+        setCount(prevCount => prevCount + 1);
+    }
+
+    const Decrease = () => {
+        setCount(prevCount => prevCount - 1);
+    }
 
     return (
         <div className="Counter">
             <div>
-                <button onClick={() => setCount(count + 1)}>Increase</button>
+                <button onClick={Increase}>Increase</button>
+                <button onClick={Decrease}>Decrease</button>
             </div>
         </div>
     )
 }
+
+export default Counter
