a43c01a5e24efc57659a3e8e87c962d28fe800aa
Sam <sam.fordham1@gmail.com>
2020-08-17 14:22:57 +0100

    Stuck on trying to connect the input to the sentence

--- src/components/greeter.js
+++ src/components/greeter.js
@@ -1,29 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Name = () => {
-    return (
-        <div>
-            <p>
-            What's your name?
-            <form onsubmit="()">
-                <input type="text" id="input" placeholder="Enter text"/>
-                <button onclick="function" type="button" id="submitbutton" value="submit">Submit</button>
-            </form> 
-            </p>
-        </div>
-    )
-}
+    const [firstName, setFirstName] = useState(``);
 
-const Greeter = () => {
-    return (
-        <div>
-            <p>
-                <Name />
-                Hello {document.getElementById("Name")} , good to see you!
-            </p>
-        </div>
-    )
-}
+    const SelectedName = () => {
+        setFirstName(firstName.onsubmit)
+    }
 
+    const Greeter = () => {
+        return (
+            <div>
+                <p>
+                What's your name?
+                <form onsubmit="()">
+                    <input type="text" id="input" placeholder="Enter text"/>
+                    <button onclick="function" type="button" id="submitbutton" value="submit">Submit</button>
+                </form> 
+                    <Name />
+                    Hello ${SelectedName}, good to see you!
+                </p>
+            </div>
+        )
+    }
+}
 
 export default Greeter
