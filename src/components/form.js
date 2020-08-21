b6af6e13eea3db8e2491aeac9cad08fb4296bbdc
Sam <sam.fordham1@gmail.com>
2020-08-21 16:56:41 +0100

    One button for all details, no useEffect

--- src/components/form.js
+++ src/components/form.js
@@ -2,44 +2,30 @@ import React, {useState, useEffect} from 'react'
 
 const Form = () => {
     const [name, setName] = useState("");
-    useEffect(() => {
-        console.log(name)
-    }, [name]
-    )
 
     const [age, setAge] = useState("");
-    useEffect(() => {
-        console.log(age)
-    }, [age]
-    )
 
     const [colour, setColour] = useState("");
-    useEffect(() => {
-        console.log(colour)
-    }, [colour]
-    )
 
     const [food, setFood] = useState("");
-    useEffect(() => {
-        console.log(food)
-    }, [food]
-    )
 
     return (
         <div>
             <form>
                 <p>What's your name?</p>
-                <input value={name} onChange={e => setName(e.target.value)}/> <button>Submit</button>
-                <p>{name}</p>
+                <input value={name} onChange={e => setName(e.target.value)}/> 
+
                 <p>How old are you?</p>
-                <input value={age} onChange={e => setAge(e.target.value)}/> <button>Submit</button>
-                <p>{age}</p>
+                <input value={age} onChange={e => setAge(e.target.value)}/> 
+
                 <p>What's your favourite colour?</p>
-                <input value={colour} onChange={e => setColour(e.target.value)}/> <button>Submit</button>
-                {colour}
+                <input value={colour} onChange={e => setColour(e.target.value)}/> 
+
                 <p>What's your favourite food?</p>
-                <input value={food} onChange={e => setFood(e.target.value)}/> <button>Submit</button>
-                <p>{food}</p>
+                <input value={food} onChange={e => setFood(e.target.value)}/> 
+                
+                <br/> <button onClick={()=>console.log({name},{age},{colour},{food})}>Submit</button>
+
             </form>
         </div>
     )
