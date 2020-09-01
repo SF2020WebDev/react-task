b13b7e2b90a08dae159b71303abd3a71b1740c4c
Sam <sam.fordham1@gmail.com>
2020-09-01 14:00:50 +0100

    Completed Component

--- src/components/questions.js
+++ src/components/questions.js
@@ -16,37 +16,37 @@ const Questions = () => {
         <div>
             <div>
                 <p>Do you live in the UK?</p>
-                <input type="radio" id="yes1" name={answerOne} value="yes" results="yes" onClick={() => setAnswerOne=="yes"}/> Yes
-                <input type="radio" id="no1" name={answerOne} value="no" results="no" onClick={() => setAnswerOne=="no"}/> No
-                <p onChange={() => complete(true) && answerOne=="yes" ? "yes" || complete(true) && answerOne=="no" ? "no" : null : null} />
+                <input type="checkbox" id="yes1" checked={answerOne==="yes"} name={answerOne} value="yes" results="yes" onClick={() => setAnswerOne("yes")}/> Yes
+                <input type="checkbox" id="no1" checked={answerOne==="no"} name={answerOne} value="no" results="no" onClick={() => setAnswerOne("no")}/> No
+                {complete ? <p>{answerOne}</p> : null}
             </div>
             <br />
             <div>
                 <p>Are you over 25?</p>
-                <input type="radio" id="yes2" name={answerTwo} value="yes" results="yes" onClick={() => setAnswerTwo=="yes"} /> Yes
-                <input type="radio" id="no2" name={answerTwo} value="no" results="no" onClick={() => setAnswerTwo=="no"}/> No
-                <p onChange={() => complete(true) && answerTwo=="yes" ? "yes" || complete(true) && answerTwo=="no" ? "no" : null : null}/>
+                <input type="checkbox" id="yes2" checked={answerTwo==="yes"} name={answerTwo} value="yes" results="yes" onClick={() => setAnswerTwo("yes")} /> Yes
+                <input type="checkbox" id="no2" checked={answerTwo==="no"} name={answerTwo} value="no" results="no" onClick={() => setAnswerTwo("no")}/> No
+                {complete ? <p>{answerTwo}</p> : null}
             </div>
             <br />
             <div>
                 <p>Do you watch the news?</p>
-                <input type="radio" id="yes3" name={answerThree} value="yes" results="yes" onClick={() => setAnswerThree=="yes"} /> Yes
-                <input type="radio" id="no3" name={answerThree} value="no" results="no" onClick={() => setAnswerThree=="no"} /> No
-                <p onChange={() => complete(true) && answerThree=="yes" ? "yes" || complete(true) && answerThree=="no" ? "no" : null : null} />
+                <input type="checkbox" id="yes3" checked={answerThree==="yes"} name={answerThree} value="yes" results="yes" onClick={() => setAnswerThree("yes")} /> Yes
+                <input type="checkbox" id="no3" checked={answerThree==="no"} name={answerThree} value="no" results="no" onClick={() => setAnswerThree("no")} /> No
+                {complete ? <p>{answerThree}</p> : null}
             </div>
             <br />
             <div>
                 <p>Do you have any allergies?</p>
-                <input type="radio" id="yes4" name={answerFour} value="yes" results="yes" onClick={() => setAnswerFour=="yes"}/> Yes
-                <input type="radio" id="no4" name={answerFour} value="no" results="no" onClick={() => setAnswerFour=="no"}/> No
-                <p onChange={() => complete(true) && answerFour=="yes" ? "yes" || complete(true) && answerFour=="no" ? "no" : null : null} />
+                <input type="checkbox" id="yes4" checked={answerFour==="yes"} name={answerFour} value="yes" results="yes" onClick={() => setAnswerFour("yes")}/> Yes
+                <input type="checkbox" id="no4" checked={answerFour==="no"} name={answerFour} value="no" results="no" onClick={() => setAnswerFour("no")}/> No
+                {complete ? <p>{answerFour}</p> : null}
             </div>
             <br />
             <div>
                 <p>Do you have any tattoos?</p>
-                <input type="radio" id="yes5" name={answerFive} value="yes" results="yes" onClick={() => setAnswerFive=="yes"} /> Yes
-                <input type="radio" id="no5" name={answerFive} value="no" results="no" onClick={() => setAnswerFive=="no"} /> No
-                <p onChange={() => complete(true) && answerFive=="yes" ? "yes" || complete(true) && answerFive=="no" ? "no" : null : null}/>
+                <input type="checkbox" id="yes5" checked={answerFive==="yes"} name={answerFive} value="yes" results="yes" onClick={() => setAnswerFive("yes")} /> Yes
+                <input type="checkbox" id="no5" checked={answerFive==="no"} name={answerFive} value="no" results="no" onClick={() => setAnswerFive("no")} /> No
+                {complete ? <p>{answerFive}</p> : null}
             </div>
             <br />
             <button onClick={()=> setComplete(true)} > Submit </button>
