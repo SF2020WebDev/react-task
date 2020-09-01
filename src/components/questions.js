3fc43bac05d16f7ceb569a16c80e31492f393ec7
Sam <sam.fordham1@gmail.com>
2020-09-01 13:05:55 +0100

    Progress so far, as of yet incomplete

--- src/components/questions.js
+++ src/components/questions.js
@@ -14,32 +14,42 @@ const Questions = () => {
     const [complete, setComplete] = useState(false)
     return (
         <div>
-            <p>Do you live in the UK?</p>
-            <input type="radio" id="yes" name="one" value={answerOne} onChange={e => setAnswerOne(e.target.value)} /> Yes
-            <input type="radio" id="no" name="one" value={answerOne} onChange={e => setAnswerOne(e.target.value)} /> No
-            <p>{answerOne}</p>
+            <div>
+                <p>Do you live in the UK?</p>
+                <input type="radio" id="yes1" name={answerOne} value="yes" results="yes" onClick={() => setAnswerOne=="yes"}/> Yes
+                <input type="radio" id="no1" name={answerOne} value="no" results="no" onClick={() => setAnswerOne=="no"}/> No
+                <p onChange={() => complete(true) && answerOne=="yes" ? "yes" || complete(true) && answerOne=="no" ? "no" : null : null} />
+            </div>
             <br />
-            <p>Are you over 25?</p>
-            <input type="radio" id="yes" name="two" value={answerTwo} onChange={e => setAnswerTwo(e.target.value)} /> Yes
-            <input type="radio" id="no" name="two" value={answerTwo} onChange={e => setAnswerTwo(e.target.value)} /> No
-            <p>{answerTwo}</p>
+            <div>
+                <p>Are you over 25?</p>
+                <input type="radio" id="yes2" name={answerTwo} value="yes" results="yes" onClick={() => setAnswerTwo=="yes"} /> Yes
+                <input type="radio" id="no2" name={answerTwo} value="no" results="no" onClick={() => setAnswerTwo=="no"}/> No
+                <p onChange={() => complete(true) && answerTwo=="yes" ? "yes" || complete(true) && answerTwo=="no" ? "no" : null : null}/>
+            </div>
             <br />
-            <p>Do you watch the news?</p>
-            <input type="radio" id="yes" name="three" value={answerThree} onChange={e => setAnswerThree(e.target.value)} /> Yes
-            <input type="radio" id="no" name="three" value={answerThree} onChange={e => setAnswerThree(e.target.value)} /> No
-            <p>{answerThree}</p>
+            <div>
+                <p>Do you watch the news?</p>
+                <input type="radio" id="yes3" name={answerThree} value="yes" results="yes" onClick={() => setAnswerThree=="yes"} /> Yes
+                <input type="radio" id="no3" name={answerThree} value="no" results="no" onClick={() => setAnswerThree=="no"} /> No
+                <p onChange={() => complete(true) && answerThree=="yes" ? "yes" || complete(true) && answerThree=="no" ? "no" : null : null} />
+            </div>
             <br />
-            <p>Do you have any allergies?</p>
-            <input type="radio" id="yes" name="four" value={answerFour} onChange={e => setAnswerFour(e.target.value)} /> Yes
-            <input type="radio" id="no" name="four" value={answerFour} onChange={e => setAnswerFour(e.target.value)} /> No
-            <p>{answerFour}</p>
+            <div>
+                <p>Do you have any allergies?</p>
+                <input type="radio" id="yes4" name={answerFour} value="yes" results="yes" onClick={() => setAnswerFour=="yes"}/> Yes
+                <input type="radio" id="no4" name={answerFour} value="no" results="no" onClick={() => setAnswerFour=="no"}/> No
+                <p onChange={() => complete(true) && answerFour=="yes" ? "yes" || complete(true) && answerFour=="no" ? "no" : null : null} />
+            </div>
             <br />
-            <p>Do you have any tattoos?</p>
-            <input type="radio" id="yes" name="five" value={answerFive} onChange={e => setAnswerFive(e.target.value)} /> Yes
-            <input type="radio" id="no" name="five" value={answerFive} onChange={e => setAnswerFive(e.target.value)} /> No
-            <p>{answerFive}</p>
+            <div>
+                <p>Do you have any tattoos?</p>
+                <input type="radio" id="yes5" name={answerFive} value="yes" results="yes" onClick={() => setAnswerFive=="yes"} /> Yes
+                <input type="radio" id="no5" name={answerFive} value="no" results="no" onClick={() => setAnswerFive=="no"} /> No
+                <p onChange={() => complete(true) && answerFive=="yes" ? "yes" || complete(true) && answerFive=="no" ? "no" : null : null}/>
+            </div>
             <br />
-            <button onClick={()=>setComplete(true)}> Submit </button>
+            <button onClick={()=> setComplete(true)} > Submit </button>
             
         </div>
     )
